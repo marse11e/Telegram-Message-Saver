c3ba23e70dd02b1fdba709c26dd7e12f8af981ef
marse11e <endless.key.2018@gmail.com>
2024-08-17 04:11:09 +0500

    U

--- core/settings.py
+++ core/settings.py
@@ -11,14 +11,11 @@ API_HASH = CONFIG.get('account', 'api_hash')
 USERNAME = CONFIG.get('account', 'username')
 PHONE = CONFIG.get('account', 'phone')
 PASSWORD = CONFIG.get('account', 'password')
-CANNEL_ID = CONFIG.get('account', 'channel_id')
-CANNEL_2_ID = CONFIG.get('account', 'channel_2_id')
 # USER_ID = CONFIG.getint('account', 'user_id')
 
-# Test
-# USER_ID_1 = CONFIG.getint('Customer_ids', 'user_id_1')
-# USER_ID_2 = CONFIG.getint('Customer_ids', 'user_id_2')
-# USER_ID_3 = CONFIG.getint('Customer_ids', 'user_id_3')
-# USER_ID_4 = CONFIG.getint('Customer_ids', 'user_id_4')
-# USER_ID_5 = CONFIG.getint('Customer_ids', 'user_id_5')
-# USER_ID_6 = CONFIG.getint('Customer_ids', 'user_id_6')
+
+# CANNEL_ID For sending private messages
+CANNEL_ID = CONFIG.get('account', 'channel_id')
+
+# CANNEL_2_ID For sending user status
+CANNEL_2_ID = CONFIG.get('account', 'channel_2_id')
